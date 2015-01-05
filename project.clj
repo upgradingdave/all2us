1b051198303811c046ddf716743301d1a68da216
upgradingdave <upgradingdave@gmail.com>
2015-01-05 16:23:16 -0500

    Initial db files

--- project.clj
+++ project.clj
@@ -1,38 +1,63 @@
-(defproject all2us "0.1.0-SNAPSHOT"
-  :description "All2Us is a url shortner"
-  :url "http://11in12.com/all2us"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [lib-noir "0.9.5"]
-                 [ring-server "0.3.1"]
-                 [selmer "0.7.7"]
-                 [com.taoensso/timbre "3.3.1"]
-                 [com.taoensso/tower "3.0.2"]
-                 [markdown-clj "0.9.58"
-                  :exclusions [com.keminglabs/cljx]]
-                 [environ "1.0.0"]
-                 [im.chit/cronj "1.4.3"]
-                 [noir-exception "0.2.3"]
-                 [prone "0.8.0"]]
-  :uberjar-name "all2us.jar"
-  :repl-options {:init-ns all2us.repl}
-  :jvm-opts ["-server"]
-  :plugins [[lein-ring "0.8.13"]
-            [lein-environ "1.0.0"]
-            [lein-ancient "0.5.5"]]
-  :ring {:handler all2us.handler/app
-         :init    all2us.handler/init
-         :destroy all2us.handler/destroy}
+(defproject
+  all2us
+  "0.1.0-SNAPSHOT"
+  :description
+  "FIXME: write description"
+  :url
+  "http://example.com/FIXME"
+  :dependencies
+  [[prone "0.8.0"]
+   [log4j
+    "1.2.17"
+    :exclusions
+    [javax.mail/mail
+     javax.jms/jms
+     com.sun.jdmk/jmxtools
+     com.sun.jmx/jmxri]]
+   [selmer "0.7.7"]
+   [com.taoensso/tower "3.0.2"]
+   [markdown-clj "0.9.58" :exclusions [com.keminglabs/cljx]]
+   [im.chit/cronj "1.4.3"]
+   [com.taoensso/timbre "3.3.1"]
+   [org.postgresql/postgresql "9.3-1102-jdbc41"]
+   [noir-exception "0.2.3"]
+   [korma "0.4.0"]
+   [lib-noir "0.9.5"]
+   [org.clojure/clojure "1.6.0"]
+   [environ "1.0.0"]
+   [ring-server "0.3.1"]
+   [ragtime "0.3.6"]]
+  :repl-options
+  {:init-ns all2us.repl}
+  :jvm-opts
+  ["-server"]
+  :plugins
+  [[lein-ring "0.8.13"]
+   [lein-environ "1.0.0"]
+   [lein-ancient "0.5.5"]
+   [ragtime/ragtime.lein "0.3.6"]]
+  :ring
+  {:handler all2us.handler/app,
+   :init all2us.handler/init,
+   :destroy all2us.handler/destroy}
   :profiles
-  {:uberjar {:omit-source true
-             :env {:production true}
-             :aot :all}
-   :production {:ring {:open-browser? false
-                       :stacktraces?  false
-                       :auto-reload?  false}}
-   :dev {:dependencies [[ring-mock "0.1.5"]
-                        [ring/ring-devel "1.3.2"]
-                        [pjstadig/humane-test-output "0.6.0"]]
-         :injections [(require 'pjstadig.humane-test-output)
-                      (pjstadig.humane-test-output/activate!)]
-         :env {:dev true}}}
+  {:uberjar {:omit-source true, :env {:production true}, :aot :all},
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}},
+   :dev
+   {:dependencies
+    [[ring-mock "0.1.5"]
+     [ring/ring-devel "1.3.2"]
+     [pjstadig/humane-test-output "0.6.0"]],
+    :injections
+    [(require 'pjstadig.humane-test-output)
+     (pjstadig.humane-test-output/activate!)],
+    :env {:dev true}}}
+  :ragtime
+  {:migrations ragtime.sql.files/migrations,
+   :database
+   "jdbc:postgresql://localhost/all2us?user=db_user_name_here&password=db_user_password_here"}
+  :uberjar-name
+  "all2us.jar"
   :min-lein-version "2.0.0")
---
+++ src/all2us/db/schema.clj
@@ -0,0 +1,9 @@
+(ns all2us.db.schema)
+
+(def db-spec
+  {:subprotocol "postgresql"
+   :subname "//localhost/all2us"
+   :user "db_user_name_here"
+   :password "db_user_password_here"})
+
+
